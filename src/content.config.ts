59a0e0ae414fcafead9b42faaaba702f25d4e6af
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-06-15 21:05:16 +0200

    work on blogging section and pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,21 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
+const blogpost = defineCollection({
 	loader: glob({ pattern: '**/*.{mdx,md}', base: './src/blogposts/' }),
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		author: z.string().default('Mikayil Gacek'),
-		image: z.object({
-			src: z.string(),
-			alt: z.string()
-		}).optional(), // TODO: Add default img ???
+		author: z.object({
+			name: z.string(),
+			img: image().optional(),
+		}).default({ name: 'Mikayil Gacek' }),
+		cover: image().optional(),
+		coverAlt: z.string().default('Blog Post Cover Image'),
 		categories: z.array(z.string())
 	})
 });
 
-export const collections = { blog };
+export const collections = { blogpost };
