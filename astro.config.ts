1538987be5448072d89cfdfb44f6d60f71708a92
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-06-21 16:16:56 +0200

    deps(unplugin-icons): add unplugin icons with lucide icons

--- astro.config.ts
+++ astro.config.ts
@@ -3,38 +3,42 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import node from '@astrojs/node';
 import { remarkReadingTime } from './src/plugins/readingTime';
-import { join, dirname } from 'path'
+import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import svelte from '@astrojs/svelte';
 import devtoolsJson from 'vite-plugin-devtools-json';
+import Icons from 'unplugin-icons/vite';
 
-const currentDir = dirname(fileURLToPath(import.meta.url))
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-    site: import.meta.env.DEV ? 'http://localhost:4321' : 'https://mikayil.dev',
-    prefetch: {
-        defaultStrategy: 'viewport',
-        prefetchAll: true
-    },
-    integrations: [mdx(), svelte(),],
-    markdown: {
-        remarkPlugins: [remarkReadingTime]
-    },
-    adapter: node({
-        mode: 'standalone'
-    }),
-    vite: {
-        plugins: [
-            devtoolsJson()
-        ],
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: "modern-compiler",
-                    additionalData: `@use "${join(currentDir, './src/assets/styles/mixins')}" as *;`
-                }
-            }
-        },
-    }
+	site: import.meta.env.DEV ? 'http://localhost:4321' : 'https://mikayil.dev',
+	prefetch: {
+		defaultStrategy: 'viewport',
+		prefetchAll: true
+	},
+	integrations: [mdx(), svelte()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime]
+	},
+	adapter: node({
+		mode: 'standalone'
+	}),
+	vite: {
+		plugins: [
+			Icons({
+				compiler: 'astro'
+			}),
+			devtoolsJson()
+		],
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: 'modern-compiler',
+					additionalData: `@use "${join(currentDir, './src/assets/styles/mixins')}" as *;`
+				}
+			}
+		}
+	}
 });
