754db6bcd3325197db31ae975e55cb2eca3ddb48
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-07-04 20:15:05 +0200

    fix: clean up transitions and animations

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -1,13 +1,15 @@
-const observer = new IntersectionObserver(
-	entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				entry.target.classList.add('visible');
-			}
-		});
-	},
-	{ threshold: 0.3 }
-);
+document.addEventListener('astro:page-load', () => {
+	const observer = new IntersectionObserver(
+		entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('visible');
+				}
+			});
+		},
+		{ threshold: 0.3 }
+	);
 
-const animatedElements = document.querySelectorAll('.animate');
-animatedElements.forEach(el => observer.observe(el));
+	const animatedElements = document.querySelectorAll('.animate');
+	animatedElements.forEach(el => observer.observe(el));
+});
